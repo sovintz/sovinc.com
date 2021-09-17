b15e8955af83868dd8ac56bb3f830eb25dc0c34c
lansovinc <lan.sovinc@gmail.com>
2021-09-17 22:43:42 +0200

    Fix Github Pages 5.

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,10 +6,10 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
-  // Router base for GitHub Pages
+  /* Router base for GitHub Pages
   router: {
     base: './'
-  },
+  }, */
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
